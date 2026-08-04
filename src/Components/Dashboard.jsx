4bd2a73c5c27943faff7e5edbde00cdffe063340
Raveena <raveena935499@gmail.com>
2026-08-04 23:54:47 +0530

    dashboard

---
+++ src/Components/Dashboard.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <>
+    <h1>DASHBOARD</h1>
+    <div className="das">
+        <div className="das1">
+            <h2> Sidebar  </h2>
+            
+        </div>
+        <div className="das2"></div>
+
+    </div>
+
+    </>
+  )
+}
+
+export default Dashboard
